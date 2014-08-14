77385a2d9ab44e47aa4d99e7f6f251bec28aac0a
theevann <evann.courdier@mines-paristech.fr>
2014-08-14 08:15:33 +0100

    First version working with csv & json files

---
+++ dataHandler.js
@@ -0,0 +1,155 @@
+﻿var computeData,
+    getData,
+    log,
+    csv = true;
+
+(function(){
+    var idCounter = 0; // Give unique id to link
+    
+    getData = function (filePath) {
+        var data, file;
+        xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET",filePath,false);
+        xmlhttp.overrideMimeType('text/plain');
+        xmlhttp.send(null);
+        //maybe check status !=404 here
+        file = xmlhttp.responseText;
+        
+        csv = (/.*\.csv/.test(filePath));
+        if(!csv && !/.*\.json/.test(filePath)){
+            log("Couldn't determine file extension\n The file '" + filePath + "' will be parsed as a csv file",true);
+            csv = true;
+        }
+        
+        if (csv) {
+            if (csvHeader !== "") {
+                if (!isNaN(parseInt(file[0]))){
+                    log("Using given csv header : " + csvHeader, true);
+                    file = csvHeader + '\n' + file;
+                }
+                else
+                    log("Aborting use of given csv header : " + csvHeader + " : The file seems to already contain a header !", true);
+            }
+            else if (!isNaN(parseInt(file[0]))) {
+                log('Header missing ! Trying to use default header ...',true);
+                file = 'timestamp,id1,id2\n' + file;
+            }
+            
+            data = d3.csv.parse(file, function(d) {
+                  return {
+                    id1: +d.id1,
+                    id2: +d.id2,
+                    timestamp: +d.timestamp
+                  };
+            });
+        }
+        else{
+            data = JSON.parse(file);
+        }
+        
+        return data;
+    };
+    
+    getComputedData = function (filePath1, filePath2) {
+        var data = {}, file1, file2;
+        var xmlhttp1 = new XMLHttpRequest();
+        xmlhttp1.open("GET",filePath1,false);
+        xmlhttp1.overrideMimeType('text/plain');
+        xmlhttp1.send(null);
+        file1 = xmlhttp1.responseText;
+        
+        var xmlhttp2 = new XMLHttpRequest();
+        xmlhttp2.open("GET",filePath2,false);
+        xmlhttp2.overrideMimeType('text/plain');
+        xmlhttp2.send(null);
+        file2 = xmlhttp2.responseText;
+        
+        if(!/.*\.json/.test(filePath1)){
+            log("The file '" + filePath1 + "' has not the expected json extension\nIt will be parsed as a json file",true);
+        }
+        if(!/.*\.json/.test(filePath2)){
+            log("The file '" + filePath2 + "' has not the expected json extension\nIt will be parsed as a json file",true);
+        }
+        csv = false;
+
+        data.nodes = JSON.parse(file1);
+        data.links = JSON.parse(file2);
+        
+        //The JSON FILES are given with the incorrect attribute 'weight' while programm is expecting 'w'
+        data.nodes.forEach(function (d) {d.w = d.weight; delete d.weight;});
+        
+        return data;
+    };
+
+    computeData = function (data) {
+        var nodes = [],
+            links = [],
+            nodesId = [],
+            linksId = [];
+        
+        //LINKS ARE NOT ORIENTED
+        log("Computing data...", true);
+        data.forEach(function(d,i){
+            nodesId = nodes.map(function (d) {return d.id;});
+            linksId = links.map(function (d) {return (d.sourceId + "-" + d.targetId);});
+            var indexNodes = [nodesId.indexOf(d.id1),nodesId.indexOf(d.id2)];
+            var indexLinks = [linksId.indexOf(d.id1 + "-" + d.id2),linksId.indexOf(d.id2 + "-" + d.id1)];
+            
+            if(i%1000 === 0)
+                log(i + " computed over " + data.length, true);
+            
+            for(var j = 0 ; j < 2 ; j++){
+                if (indexNodes[j] >= 0) {
+                    nodes[indexNodes[j]].w += 1;
+                }
+                else {
+                    var newNode = {
+                        id : d["id"+(j+1)],
+                        w : 1,
+                        currentW : 0
+                    };
+                    nodes.push(newNode);
+                }
+            }
+            
+            
+            if (indexLinks[0] >= 0) {
+                if(links[indexLinks[0]].timestamps.indexOf(d.timestamp) === -1)
+                    links[indexLinks[0]].timestamps.push(d.timestamp);
+            }
+            else if (indexLinks[1] >= 0){
+                if(links[indexLinks[1]].timestamps.indexOf(d.timestamp) === -1)
+                    links[indexLinks[1]].timestamps.push(d.timestamp);
+            }
+            else {
+                var newLink = {
+                    sourceId : d.id1,
+                    targetId : d.id2,
+                    timestamps : [d.timestamp],
+                    id : idCounter++,
+                    currentW : 0
+                };
+                links.push(newLink);
+            }
+        });
+        
+        nodesId = nodes.map(function (d) {return d.id;});
+        
+        links.forEach(function (d) {
+            d.source = nodesId.indexOf(d.sourceId);
+            d.target = nodesId.indexOf(d.targetId);
+            d.timestamps.sort(function(a, b) {
+                return a - b;
+            });
+        });
+        
+        maxW = d3.max(nodes, function (d) {return d.w;});
+        
+        return {nodes : nodes, links : links};
+    };
+    
+    log = function(message, higherPriority){
+        if(showLog && ((logLevel === 'limited' && higherPriority === true) || (logLevel === 'all')))
+            console.log(message);
+    };
+})();
--- main.js
+++ main.js
@@ -1,105 +1,279 @@
-﻿var filePath = "graph.csv";
-xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET",filePath,false);
-xmlhttp.overrideMimeType('text/plain');
-xmlhttp.send(null);
-//maybe check status !=404 here
-var file = xmlhttp.responseText;
+﻿var init,
+    update,
+    csvHeader,
+    showLog,
+    logLevel;
 
-
-var width = 1500,
-    height = 800,
-    maxW = 0,
-    idCounter = 0,
-    maxNodeSize = 30, // In pixel
-    window = 1800, // Window size
-    currentTime = 0, // Beggining of the window
-    step = 1,   // Step time the window is moving
-    currentNodes = [], // Nodes in window
-    currentLinks = []; // Links in window
-
-var svg = d3.select("#graph").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var force = d3.layout.force()
-    .gravity(.2)
-    .linkDistance(200)
-    .linkStrength(0.5)
-    .charge(-2000)
-    .size([width, height]);
+(function(){
     
-var data = d3.csv.parse(file, function(d) {
-  return {
-    id1: +d.id1,
-    id2: +d.id2,
-    timestamp: +d.timestamp
-  };
-});
-
-var computedData = computeData1(data);
-console.log(computedData);
-
-
-
-  force
-      .nodes(computedData.nodes)
-      .links(computedData.links)
-      .start();
-
-  var link = svg.selectAll(".link")
-      .data(computedData.links)
-    .enter().append("line")
-      .attr("class", "link");
-
-  var node = svg.selectAll(".node")
-      .data(computedData.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
-
-  node.append("image")
-      .attr("xlink:href", "https://github.com/favicon.ico")
-      .attr("x", -maxNodeSize/2)
-      .attr("y", -maxNodeSize/2)
-      .attr("width", function (d) {return 5+d.w/maxW*maxNodeSize})
-      .attr("height", function (d) {return 5+d.w/maxW*maxNodeSize});
-
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.id });
-
-      
-      
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
-
-  
-    var update = function () {
-        currentTime += step;
-        currentNodes = getNodesInWindow(currentTime, (currentTime + window), computedData);
+    //You may specify a csv header if your csv doesn't have one : It must contains the variables 'id1', 'id2', 'timestamp' ; default is:
+    //csvHeader = "timeStamp,id1,id2"
+    csvHeader = "";
+    
+    // Log properties
+    showLog = true; // Show log
+    logLevel = 'limited'; // You can choose 'all' / 'limited'
+    
+    //Parameters - may be modified
+    var width = 1500,
+        height = 800,
         
-        force.start();
-    }
-  
-    var getNodesInWindow = function (startTime, endTime, allData) {
-        var linkId = currentLinks.map(function (d) {return d.id;})
-            arrayList = [],
-            i = 0;
-            //bisect = d3.bisector(function(d) { return d; }).left;
-            
-        allData.links.forEach(function(d,i){
-            if(d.timestamp[0] < endTime && d.timestamp[d.timestamp.length] > startTime)
-                (i = linkId.indexOf(d.id)) == -1 ? currentLinks[]
-            else if ()
+        minLinkDistance = 200, // In pixel
+        maxLinkDistance = 400, // In pixel
+        
+        minLinkSize = 0.5, // The stroke-width (in pixel probably)
+        maxLinkSize = 4, // The stroke-width (in pixel probably)
+        maxNodeSize = 50, // In pixel
+        minNodeSize = 5, // In pixel
+        
+        // This depends on the dataset:
+        currentTime = 0, // Beggining of the time-window
+        step = 20,   // Step time the time-window is moving
+        windowSize = 300, // TIME-Window size 
+        
+        // Animated graph properties :
+        animate = false, // To start animation
+        animationStep = 100, // Time in ms between each update of the time window (i.e between each currentTime = currentTime + step)
+        
+        animationOnChanging = true; // Show a circle widening/shrinking to the position of the created/removed node
+    
+    /*
+    *   == PROGRAM BEGGINING ==
+    *   written by Evann Courdier
+    */
+    
+    //Program variables - do not touch
+    var link, node, computedData, allNodesId, allLinksId, svg, force;
+
+    var currentNodeMaxWeight = 0,
+        currentLinkMaxWeight = 0,
+        currentNodes = [], // Nodes in window
+        currentLinks = []; // Links in window
+        
+        
+    svg = d3.select("#graph").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    force = d3.layout.force()
+        .gravity(0.2)
+        .linkDistance(function(d){return maxLinkDistance + d.currentW / currentLinkMaxWeight * (minLinkDistance - maxLinkDistance);})
+        .linkStrength(0.5)
+        .friction(0.5)
+        .charge(-2000)
+        .size([width, height]); 
+
+    init = function(filePath1, filePath2){
+        var data;
+        data = (filePath2 !== undefined) ? getComputedData(filePath1, filePath2) : getData(filePath1); // Load file(s)
+        
+        computedData = csv ? computeData(data) : data; // Compute it if necessary (i.e if it's not json)
+        allNodesId = computedData.nodes.map(function (d) {return d.id;});
+        allLinksId = computedData.links.map(function (d) {return d.id;});
+        
+        link = svg.selectAll(".link")
+            .data(currentLinks, function(d) {return d.id;});
+
+        node = svg.selectAll(".node")
+            .data(currentNodes, function(d) {return d.id;});
+        
+        //Define the tick method to call
+        force.on("tick", function() {
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+            node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
         });
-    }
+        
+        log("Displaying graph...", true);
+        
+        setTimeout(
+            function() { update(); },
+            3000
+        );
+
+    }; 
+ 
+    update = function () {
+        force.stop();
+        //Update the currentLinks and currentNodes variables with the new window
+        updateCurrentData(currentTime, (currentTime + windowSize), computedData);        
+        //Move the window
+        currentTime += step;
+
+        
+        //Updating Links
+        link = link.data(currentLinks, function(d) {return d.id;});
+        
+        link.enter().insert("line", ".node")
+            .attr("class", "link");
+        
+        link.attr("stroke-width", function (d) { return minLinkSize + d.currentW / currentLinkMaxWeight * (maxLinkSize - minLinkSize); });
+        
+        link.exit().remove();
+        
+        //Updating Nodes
+        node = node.data(currentNodes, function(d) {return d.id;});
+        
+        node.selectAll("image")
+            .attr("x", function (d) {return -getNodeSize(d) / 2; })
+            .attr("y", function (d) {return -getNodeSize(d) / 2; })
+            .attr("width", getNodeSize)
+            .attr("height", getNodeSize);
+        
+        node.selectAll("text")
+            .text(function(d) { return d.id + " (" + d.currentW + ")"; });
+        
+        var newNode = node.enter();
+        
+        newNode = newNode.append("g")
+            .attr("class", "node")
+            .call(force.drag);
+        
+        newNode.append("image")
+            .attr("xlink:href", "https://github.com/favicon.ico")
+            .attr("x", function (d) {return -getNodeSize(d) / 2; })
+            .attr("y", function (d) {return -getNodeSize(d) / 2; })
+            .attr("width", getNodeSize)
+            .attr("height", getNodeSize);
+        
+        newNode.append("text")
+              .attr("dx", function (d) {return 5 + getNodeSize(d) / 2; })
+              .attr("dy", ".35em")
+              .text(function(d) { return d.id + " (" + d.currentW + ")"; });
+        
+        if(animationOnChanging){
+            newNode.append("circle")
+                .attr("fill","#bbb")
+                .attr("stroke","none")
+                .attr("cx",0)
+                .attr("cy",0)
+                .attr("r",0)
+                .attr("opacity",0.7)
+                .transition().duration(800)
+                .attr("r", 150)
+                .attr("opacity",0)
+                .each("end",function() {
+                    d3.select(this).remove();
+                });
+            
+            node.exit().append("circle")
+                .attr("fill","#bbb")
+                .attr("stroke","none")
+                .attr("cx",0)
+                .attr("cy",0)
+                .attr("r",150)
+                .attr("opacity",0)
+                .transition().duration(800)
+                .attr("r", 0)
+                .attr("opacity",0.7)
+                .each("end",function() {
+                    d3.select(this).remove();
+                });
+                
+            node.exit().transition().duration(800)
+                .remove();
+        } else {
+            node.exit().remove();
+        }
+        
+        //Update the force
+        force
+            .nodes(currentNodes)
+            .links(currentLinks);
+        //Restart it
+        force.start();
+        
+        //Launch the next step
+        if(animate){
+            setTimeout(
+                function() { update(); },
+                animationStep
+            );
+        }
+    };
+    
+    var getNodeSize = function(d){
+        return minNodeSize + d.currentW / currentNodeMaxWeight * (maxNodeSize - minNodeSize);
+    };
   
-  //*/
+    var updateCurrentData = function (startTime, endTime, allData) { // TO DO : update link weight too
+        var linkId = currentLinks.map(function (d) {return d.id;}),
+            nodeId = currentNodes.map(function (d) {return d.id;}),
+            j = 0,
+            n1 = 0,
+            n2 = 0,
+            idx = 0,
+            newNode = {},
+            changingW = 0;
+            
+        allData.links.forEach(function(d){
+            j = linkId.indexOf(d.id);
+            changingW = -d.currentW + (d.currentW = (d3.bisectRight(d.timestamps, endTime) - d3.bisectLeft(d.timestamps, startTime))); // Compute the difference between previous and new weight of the link on the time window
+            
+            if (d.currentW !== 0 && j === -1) { // If the link has to exist but doesn't exist
+                    currentLinks.push(d);
+                    linkId.push(d.id);
+                    log("Link created (" + d.id + ") : " + d.sourceId + " <=> " + d.targetId);
+                    
+                    //Then we have to add the nodes if they didn't exist
+                    n1 = nodeId.indexOf(d.sourceId);                    
+                    if (n1 === -1) {
+                        idx = allNodesId.indexOf(d.sourceId);
+                        newNode = allData.nodes[idx];
+                        currentNodes.push(newNode);
+                        nodeId.push(d.sourceId);
+                        log("Node created : " + d.sourceId);
+                    }
+                    
+                    n2 = nodeId.indexOf(d.targetId);
+                    if (n2 === -1) {
+                        idx = allNodesId.indexOf(d.targetId);
+                        newNode = allData.nodes[idx];
+                        currentNodes.push(newNode);
+                        nodeId.push(d.targetId);
+                        log("Node created : " + d.targetId);
+                    }
+                  
+            } 
+            else if (d.currentW === 0 && j !== -1) { // If the link shouldn't exist but exist
+                currentLinks.splice(j,1);
+                linkId.splice(j,1);
+                log("Link removed (" + d.id + ") : " + d.sourceId + " <=> " + d.targetId);
+                
+                //Then we have to remove the node and update the current Weight
+                n1 = nodeId.indexOf(d.sourceId);              
+                if (currentNodes[n1].currentW + changingW === 0) { // if the node is linked with only one other node, then remove it
+                    currentNodes.splice(n1,1);
+                    nodeId.splice(n1,1);
+                    log("Node removed : " + d.sourceId);
+                }
+                
+                n2 = nodeId.indexOf(d.targetId); 
+                if (currentNodes[n2].currentW + changingW === 0) { // Idem
+                    currentNodes.splice(n2,1);
+                    nodeId.splice(n2,1);
+                    log("Node removed : " + d.targetId);
+                }
+            }
+            
+            n1 = allNodesId.indexOf(d.sourceId);
+            n2 = allNodesId.indexOf(d.targetId);
+            allData.nodes[n1].currentW += changingW;
+            allData.nodes[n2].currentW += changingW;
+        });
+        
+        //Link the source and target with the current position if the nodes in the node array
+        currentLinks.forEach(function(d){
+            d.source = nodeId.indexOf(d.sourceId);
+            d.target = nodeId.indexOf(d.targetId);
+        });
+        
+        //Update the current maximum weight
+        currentNodeMaxWeight = d3.max(currentNodes, function(d) {return d.currentW;});
+        currentLinkMaxWeight = d3.max(currentLinks, function(d) {return d.currentW;});
+
+    };
+})();
